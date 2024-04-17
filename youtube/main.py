e2978ad91952a6396030d6f4978400453bd3f6fd
reverse <att4ck07@gmail.com>
2024-04-17 23:54:04 +0000

    ehmd

--- youtube/main.py
+++ youtube/main.py
@@ -1,8 +1,15 @@
 from src.bot.irc_bot import CustomYouTubeBot
+from src.config.configuration import ConfigManager
 
 def main():
-    bot = CustomYouTubeBot()
+    config_manager = ConfigManager()
+    irc_settings = config_manager.get_irc_settings()
+    server, port, channel, bot_name, password = irc_settings
+    api_key = config_manager.get_youtube_api_key()
+    bot = CustomYouTubeBot(server, port, channel, bot_name, api_key=api_key, password=password)
     bot.start()
 
 if __name__ == "__main__":
     main()
+
+
--- youtube/src/config/configuration.py
+++ youtube/src/config/configuration.py
@@ -1,18 +1,18 @@
 import configparser
 
 class ConfigManager:
-    def __init__(self):
+    def __init__(self, config_file='config.ini'):
         self.config = configparser.ConfigParser()
-        self.config.read('config.ini')
+        self.config.read(config_file)
 
     def get_irc_settings(self):
-        return (
-            self.config.get('IRCSettings', 'server'),
-            self.config.getint('IRCSettings', 'port'),
-            self.config.get('IRCSettings', 'channel'),
-            self.config.get('IRCSettings', 'bot_name'),
-            [self.config.get('AuthorizedUsers', key) for key in self.config['AuthorizedUsers']]
-        )
+        server = self.config.get('IRCSettings', 'server')
+        port = self.config.getint('IRCSettings', 'port')
+        channel = self.config.get('IRCSettings', 'channel')
+        bot_name = self.config.get('IRCSettings', 'bot_name')
+        authorized_users = [self.config.get('AuthorizedUsers', key) for key in self.config['AuthorizedUsers']]
+        return server, port, channel, bot_name, authorized_users
 
     def get_youtube_api_key(self):
         return self.config.get('YouTubeAPI', 'api_key')
+
