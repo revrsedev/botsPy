77a7d97da4569f2c2d25f6488d39957d849fb29b
Debian <debian@vps-9f1a1108.vps.ovh.net>
2024-04-17 13:59:38 +0000

    fix to listen in all chan the bot is in & better PING PONG manage

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -24,24 +24,31 @@ youtube_pattern = r"(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/
 class YouTubeBot(irc.bot.SingleServerIRCBot):
     def __init__(self):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], bot_name, bot_name)
+        self.channels = {}
         self.ping_thread = threading.Thread(target=self.ping_server, daemon=True)
         self.ping_thread.start()
 
     def ping_server(self):
         while True:
             if self.connection.is_connected():
-                self.connection.ping("ping")
-                self.connection.process_once()
-            time.sleep(300)  # Send PING every 5 minutes
+                self.send_ping()
+            time.sleep(200)  # Send PING 
+
+    def send_ping(self):
+        self.connection.ping("ping")
+        
+    def on_pong(self, connection, event):
+        print("Received PONG response from server.")
 
     def on_welcome(self, connection, event):
         connection.join(channel)
+        self.channels[channel] = {}
 
     def on_pubmsg(self, connection, event):
         msg = event.arguments[0]
         youtube_links = re.findall(youtube_pattern, msg)
         if youtube_links:
-            self.process_youtube_links(connection, youtube_links)
+            self.process_youtube_links(connection, youtube_links, event.target)
 
     def on_privmsg(self, connection, event):
         nick = event.source.nick
@@ -51,17 +58,20 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
         if hostmask in authorized_users:
             if msg.startswith("!leave"):
                 channel_name = msg.split()[1]
-                connection.part(channel_name)
+                connection.part(channel_name, f"Leaving channel {channel_name} requested by {nick}.")
                 print(f"Leaving channel {channel_name}")
             elif msg.startswith("!join"):
                 channel_name = msg.split()[1]
-                connection.join(channel_name)
-                print(f"Joining channel {channel_name}")
+                if channel_name in self.channels:
+                    connection.privmsg(nick, f"I'm already on {channel_name}.")
+                else:
+                    connection.join(channel_name)
+                    self.channels[channel_name] = {}
+                    print(f"Joining channel {channel_name}")
         else:
             print(f"Unauthorized attempt by {hostmask} to use leave or join command.")
 
-
-    def process_youtube_links(self, connection, youtube_links):
+    def process_youtube_links(self, connection, youtube_links, target_channel):
         for video_id in youtube_links:
             response = requests.get(f"https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id={video_id}&key={api_key}")
             if response.status_code == 200:
@@ -76,8 +86,8 @@ class YouTubeBot(irc.bot.SingleServerIRCBot):
                     minutes, seconds = divmod(duration_human.total_seconds(), 60)
                     duration_str = f"{int(minutes)}m{int(seconds)}s"
                     views = statistics.get("viewCount", "N/A")
-                    output_message = f"Video: {title} - Duration: {duration_str} - Views: {views}"
-                    connection.privmsg(channel, output_message)
+                    output_message = f"\x02\x0301,00You\x0F\x02\x0300,04Tube\x0F: {title} - Duración: {duration_str} - Visto: {views}"
+                    connection.privmsg(target_channel, output_message)
             else:
                 print("Error fetching video information.")
 
